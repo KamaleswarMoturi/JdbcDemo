98ba8e049ee670e8c6a0ee88803eb7ed1f5a7e90
kamaleswar Moturi <kamaleswarmoturi@gmail.com>
2022-08-01 15:29:01 +0530

    insert feature added

--- StudentDao.java
+++ StudentDao.java
@@ -1,6 +1,23 @@
 import java.sql.*;
 public class StudentDao {
-	
+	public static boolean insertStudentToDB(Student st) {
+		boolean f=false;
+		try {
+			Connection conn=ConnectionProvider.createC();
+			String q="insert into students(name,phone,city) values(?,?,?)";
+			PreparedStatement pstmt=conn.prepareStatement(q);
+			pstmt.setString(1, st.getStudentName());
+			pstmt.setString(2, st.getStudentPhone());
+			pstmt.setString(3, st.getStudentCity());
+			pstmt.executeUpdate();
+			f=true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return f;
+	}
+
 	
 }
  
